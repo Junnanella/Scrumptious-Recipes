8733c0a9dbe088be8ddfe1a1eb31990bf44a0c4b
Junella Caringal <junellacaringal@gmail.com>
2022-04-20 11:58:34 -0700

    Created and migrated Recipe model in models.py

--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,16 @@ from django.db import models
 
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    """the __str__ method, is what gets called by Python when it wants to turn it into a string. 
+    You can use any of the properties that you defined in the __str__ method."""
+
+    def __str__(self):
+        return self.name
