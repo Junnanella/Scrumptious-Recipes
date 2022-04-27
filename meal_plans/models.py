4e7a344e373207f0cc7963c8e1013a22aed7b4cb
Junella Caringal <junellacaringal@gmail.com>
2022-04-27 11:49:50 -0700

    Create MealPlan app and model. Added blank html templates for new app.

---
+++ meal_plans/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+USER_MODEL = settings.AUTH_USER_MODEL
+
+# Create your models here.
+class MealPlan(models.Model):
+    name = models.CharField(max_length=120)
+    date = models.DateTimeField()
+    owner = models.ForeignKey(
+        USER_MODEL, related_name="meal_plan", on_delete=models.CASCADE
+    )
+    recipes = models.ManyToManyField("recipes.Recipe", related_name="meal_plan")
