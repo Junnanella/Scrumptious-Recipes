7d4094420e9d9d33f8fd9925ecd8a54604b7d96a
Junella Caringal <junellacaringal@gmail.com>
2022-04-27 12:20:20 -0700

    Created Views. Added path to meal_plans in project urls.py. Created url paths for app. Able to load meal plan list template on browser.

---
+++ meal_plans/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from meal_plans.views import (
+    MealPlanCreateView,
+    MealPlanDeleteView,
+    MealPlanUpdateView,
+    MealPlanDetailView,
+    MealPlanListView,
+)
+
+urlpatterns = [
+    path("", MealPlanListView.as_view(), name="meal_plans_list"),
+    # path("", MealPlanDetailView.as_view(), name="meal_plan_detail"),
+    # path("", MealPlanCreateView.as_view(), name="meal_plan_new"),
+    # path("", MealPlanUpdateView.as_view(), name="meal_plan_edit"),
+    # path("", MealPlanDeleteView.as_view(), name="meal_plan_delete"),
+]
--- meal_plans/views.py
+++ meal_plans/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from audioop import reverse
+from django.shortcuts import redirect
+from meal_plans.models import MealPlan
+from django.views.generic import (
+    ListView,
+    DetailView,
+    UpdateView,
+    CreateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+
+class MealPlanListView(ListView):
+    model = MealPlan
+    template_name = "meal_plans/list.html"
+    paginate_by = 2
+
+
+class MealPlanDetailView(DetailView):
+    model = MealPlan
+    template_name = "meal_plans/detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class MealPlanCreateView(LoginRequiredMixin, CreateView):
+    model = MealPlan
+    template_name = "meal_plans/new.html"
+    fields = ["name", "description"]
+
+    success_url = reverse_lazy("meal_plans_list")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class MealPlanUpdateView(LoginRequiredMixin, UpdateView):
+    model = MealPlan
+    template_name = "meal_plans/edit.html"
+    fields = ["name", "description"]
+
+    success_url = reverse_lazy("meal_plans_list")
+
+
+class MealPlanDeleteView(LoginRequiredMixin, DeleteView):
+    model = MealPlan
+    template_name = "meal_plans/delete.html"
+    success_url = reverse_lazy("meal_plans_list")
